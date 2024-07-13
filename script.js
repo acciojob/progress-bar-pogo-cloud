f9f493bfb1bbaafddbd1f79137c71bdf31f25373
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-07-13 19:59:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+ let previousbut = document.querySelector("#prev");
+        let nextbut = document.querySelector("#next");
+        let circle= document.querySelector(".circle");
+         let activeCircle=1;
+        document.querySelector("#circle-1").classList.add("active");
+         document.querySelector("#line-1").classList.add("active");
+        nextbut.addEventListener("click" ,() => {
+			document.querySelector(`#circle-${activeCircle}`).classList.remove("active");
+			document.querySelector(`#line-${activeCircle}`).classList.remove("active");
+			activeCircle++;
+			document.querySelector(`#circle-${activeCircle}`).classList.add("active");
+			document.querySelector(`#line-${activeCircle}`).classList.add("active");
+			
+			
+			
+        })
